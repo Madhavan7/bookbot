c9ce9898dfabe2772954315b04de4ac2fc214ad3
Madhavan7 <madhav.gopakumar@mail.utoronto.ca>
2023-04-22 19:16:53 -0400

    done

---
+++ main.py
@@ -0,0 +1,12 @@
+print("Hello World")
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    print(text)
+
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+main()
