0af7ac62759247ddd8d6d104bab6f0a284e3c914
Madhavan7 <madhav.gopakumar@mail.utoronto.ca>
2023-04-23 12:30:47 -0400

    done

--- main.py
+++ main.py
@@ -16,15 +16,25 @@ def num_letters():
     letters = {}
     for word in words:
       for letter in word:
+        if letter.isalpha():
           if letter.lower() in letters:
             letters[letter.lower()] += 1
           else:
             letters[letter.lower()] = 1
-    print(letters)
     return letters
+
+def get_report():
+  num = num_words()
+  letters = num_letters()
+  letters_array = [(letter, letters[letter]) for letter in letters]
+  letters_array.sort(reverse=True, key = lambda x:x[1])
+  print("--- Begin report of books/frankenstein.txt ---")
+  print(str(num) + " words found in the document\n")
+  for i in range(len(letters_array)):
+    print("The \'" + letters_array[i][0] + "\' character was found " + str(letters_array[i][1]) + " times")
   
 def get_book_text(path):
     with open(path) as f:
         return f.read()
 
-num_letters()
+get_report()
