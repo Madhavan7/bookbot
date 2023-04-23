a9994c3cecc3ecb0c8068e40b58deb196fdf39a6
Madhavan7 <madhav.gopakumar@mail.utoronto.ca>
2023-04-22 22:01:13 -0400

    num_letters

--- main.py
+++ main.py
@@ -1,12 +1,30 @@
-print("Hello World")
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     print(text)
 
+def num_words():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    words = text.split()
+    return len(words)
 
+def num_letters():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    words = text.split()
+    letters = {}
+    for word in words:
+      for letter in word:
+          if letter.lower() in letters:
+            letters[letter.lower()] += 1
+          else:
+            letters[letter.lower()] = 1
+    print(letters)
+    return letters
+  
 def get_book_text(path):
     with open(path) as f:
         return f.read()
 
-main()
+num_letters()
